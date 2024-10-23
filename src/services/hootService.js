ea30a8557a662f7ccfcdf407dec8dcbce5da0ed1
zjyezaire <zaire.elleby@gmail.com>
2024-10-23 12:28:53 -0400

    added new create hoot

---
+++ src/services/hootService.js
@@ -0,0 +1,17 @@
+const create = async (hootFormData) => {
+  try {
+    const res = await fetch(BASE_URL, {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('token')}`,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(hootFormData),
+    });
+    return res.json();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export { index, show, create };
