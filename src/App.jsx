b9447795f2de4d795524d181fe72ba9b1c14ceba
MDeadwiler <mdeadwiler@email.com>
2024-10-23 12:37:03 -0400

    added-env

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { useState, useEffect, createContext } from "react";
 import { Routes, Route } from "react-router-dom";
-import { getUser, signout } from "./services/authService.js";
-import NavBar from "./components/Navbar.jsx";
-import Landing from "./components/Landing.jsx";
-import Dashboard from "./components/Dashboard.jsx";
-import SignupForm from "./components/SignupForm.jsx";
-import SigninForm from "./components/SigninForm.jsx";
+import { signup, signin, signout, getUser } from "./components/services/authService.js"
+import NavBar from "./components/NavBar/NavBar.jsx";
+import Landing from "./components/Landing/Landing.jsx";
+import Dashboard from "./components/Dashboard/Dashboard.jsx";
+import SignupForm from "./components/SignupForm/SignupForm.jsx";
+import SigninForm from "./components/SigninForm/SigninForm.jsx";
 import "./App.css";
 
 export const AuthedUserContext = createContext(null);
--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -3,7 +3,7 @@
 const Dashboard = ({ user }) => {
     return (
       <main>
-        <h1>Welcome, {user.username}</h1>
+        <h1>Welcome, {user}</h1>
         <p>
           This is the dashboard page where you, and only you, can see a dashboard
           of all of your things.
